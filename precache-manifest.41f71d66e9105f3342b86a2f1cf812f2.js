e14e3fb72fe83fe0c59a41b95fc8781f8e381025
Julio Denis <julio_denis@live.com>
2020-10-17 18:36:22 -0500

    Updates

---
+++ precache-manifest.41f71d66e9105f3342b86a2f1cf812f2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc9a5e915e40f9fe14ad6711cbde11c",
+    "url": "/my-web/index.html"
+  },
+  {
+    "revision": "4f2519d061adb193e750",
+    "url": "/my-web/static/css/main.2383e393.chunk.css"
+  },
+  {
+    "revision": "ecd99763330072b7df87",
+    "url": "/my-web/static/js/2.a221cc66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-web/static/js/2.a221cc66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2519d061adb193e750",
+    "url": "/my-web/static/js/main.9a88eb3f.chunk.js"
+  },
+  {
+    "revision": "88dc7dd9edb1df4e869f",
+    "url": "/my-web/static/js/runtime-main.55fd68ff.js"
+  },
+  {
+    "revision": "a1945b088d9a42de3933ef7425adfcb7",
+    "url": "/my-web/static/media/JD.a1945b08.svg"
+  },
+  {
+    "revision": "b32cf20d7badf713124cbc7672b5d270",
+    "url": "/my-web/static/media/expressLogo.b32cf20d.svg"
+  },
+  {
+    "revision": "5ca42a1b2e4117a81965db525449e29f",
+    "url": "/my-web/static/media/htmlLogo.5ca42a1b.svg"
+  },
+  {
+    "revision": "2c3c135cc8e06e14eb916df38e89b3fb",
+    "url": "/my-web/static/media/jestLogo.2c3c135c.svg"
+  },
+  {
+    "revision": "ffec4afd50d8d029da7ec2863ad22d53",
+    "url": "/my-web/static/media/jsLogo.ffec4afd.svg"
+  },
+  {
+    "revision": "88303fd1e62f73d1190c59b4217be56b",
+    "url": "/my-web/static/media/julio.88303fd1.jpg"
+  },
+  {
+    "revision": "e4af772bc1cdcc731cf58ef71e4c0b54",
+    "url": "/my-web/static/media/mongoLogo.e4af772b.svg"
+  },
+  {
+    "revision": "ec3a841ead677fa03cc8ade2c6f2017a",
+    "url": "/my-web/static/media/nodeLogo.ec3a841e.svg"
+  },
+  {
+    "revision": "98aca704dc48d7e51acdea71838905c9",
+    "url": "/my-web/static/media/reactLogo.98aca704.svg"
+  },
+  {
+    "revision": "5fa12df3ddfd1dcf98cabf03e061c7fb",
+    "url": "/my-web/static/media/sassLogo.5fa12df3.svg"
+  }
+]);
